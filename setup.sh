8576e446b9557179e5c9e286e42657f46d19a07f
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-02-28 11:11:40 +0100

    first two apps

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+echo $(python3 -V)
+rm -rf venv
+mkdir -p venv
+python3 -m venv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+
---
+++ src/book_visualizer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookVisualizerConfig(AppConfig):
+    name = 'book_visualizer'
