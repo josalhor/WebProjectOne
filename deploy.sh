7618a0bef69df76a3c056a418dfef8e11159017c
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-03-07 13:21:38 +0100

    deploy travis as script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker tag joshsalvia/webprojectone registry.heroku.com/$HEROKU_APP_NAME/web
+docker push joshsalvia/webprojectone
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:release web --app $HEROKU_APP_NAME
