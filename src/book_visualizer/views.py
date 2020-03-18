afd494789ac363fc8fab260dcec73acaa9b141e8
bcaspi <balcp11@hotmail.com>
2020-03-18 13:06:26 +0100

    small fix

--- src/book_visualizer/views.py
+++ src/book_visualizer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import UserCreationForm
+from .models import Book
 
 class SignUp(generic.CreateView):
 	form_class = UserCreationForm
@@ -11,3 +12,12 @@ class SignUp(generic.CreateView):
 # We are using reverse_lazy because for all generic class-based views the urls
 # are not loaded when the file is imported, so we have to use the lazy form of
 # reverse to load them later when they are available.
+
+def bestsellers_list(request):
+	bestsellers = Book.objects.all()
+
+	context = {
+		'bestsellers': bestsellers
+	}
+
+	return render(request, 'home.html', context)
