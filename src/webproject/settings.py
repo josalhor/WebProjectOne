c51051fe7c144917da25d1cf4345a59a4d273fd4
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-03-19 19:14:43 +0100

    print db settings

--- src/webproject/settings.py
+++ src/webproject/settings.py
@@ -94,6 +94,7 @@ DATABASES = {
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
+print('DB SETTINGS:', DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
