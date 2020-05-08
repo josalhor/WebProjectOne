b3a4ca7e5d6a71fab7fb1d897da059f36ff8c6ad
Anna Torres Tuca <annattmail@gmail.com>
2020-05-08 09:15:17 +0200

    Extended testing authentication

--- features/authentication.feature
+++ features/authentication.feature
@@ -1,6 +1,23 @@
 Feature: User can login and use authentication
 
-    Scenario: User can login
-        Given Exists a user "username" with password "password"
+    Scenario: Registered user can login
+        Given Exists a user "username" with password "testpass123"
+        When I login as user "username" with password "testpass123"
+        Then I should be redirected to account url
+
+    Scenario: Not registerd username cannot login
+        Given Not registered username "username"
         When I login as user "username" with password "password"
-        Then I should see "Hi username"
+        Then I see a message error
+
+    Scenario: Registerd username types wrong password and cannot login
+        Given Exists a user "username" with password "password"
+        When I login as user "username" with password "wrongpassword"
+        Then I see a message error
+
+    Scenario: Not registerd username can sign up
+        Given Not registered username "test"
+        When I go to signup url
+        And I signup as user "test" with password "userpass123"
+        Then I am redirected to the login form
+
--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -2,23 +2,51 @@ from behave import *
 
 use_step_matcher("parse")
 
-@given('Exists a user "{username}" with password "{password}"')
+@given(u'Exists a user {username} with password {password}')
 def step_impl(context, username, password):
-    from django.contrib.auth.models import User
-    u = User(username=username, email='test@test.com')
-    u.set_password(password)
+    from book_visualizer.models import User
+    User.objects.create_user(username=username, email='user@example.com', password=password)
 
-@when('I login as user "{username}" with password "{password}"')
+@given(u'Not registered username {username}')
+def step_impl(context, username):
+    #from django.contrib.auth.models import User
+    #if(username.User.Exists):
+        #eliminar usuari
+        #pass
+    #else:
+        pass
+
+@when(u'I login as user {username} with password {password}')
 def step_impl(context, username, password):
     context.browser.visit(context.get_url('/login/'))
-    form = context.browser.find_by_tag('form').first
+    form = context.browser.find_by_tag('form')[1]
     context.browser.fill('username', username)
     context.browser.fill('password', password)
-    form.find_by_value('Login').first.click()
+    form.find_by_tag('button').first.click()
 
-@then(u'I should see "Hi username"')
+@when(u'I go to signup url')
 def step_impl(context):
-    br = context.browser
-    response = br.response()
-    assert response.code == 200
-    assert context.browser.is_text_present('Hi username')
+    context.browser.visit(context.get_url('/sign_up/'))
+
+@when(u'I signup as user {username} with password {password}')
+def step_impl(context, username, password):
+    form = context.browser.find_by_tag('form')[1]
+    context.browser.fill('username', username)
+    context.browser.fill('email', "username@test.com")
+    context.browser.fill('password1', password)
+    context.browser.fill('password2', password)
+    form.find_by_tag('button').first.click()
+
+@then(u'I should be redirected to account url')
+def step_impl(context, username):
+    assert context.browser.url.startswith(context.get_url('/account/'))
+    assert context.browser.is_text_present("Hi")
+
+@then(u'I see a message error')
+def step_impl(context):
+    assert context.browser.is_text_present("Invalid username and/or password.")
+
+@then(u'I am redirected to the login form')
+def step_impl(context):
+    print(context.browser.url)
+    assert context.browser.url.startswith(context.get_url('/login/')) #pending to fix. expected login but got signup
