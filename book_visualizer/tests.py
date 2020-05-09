0e1754fc7c449705f164ae0d350569eaa5e5f250
Sara Quejido <quejidosara@gmail.com>
2020-05-10 01:15:26 +0200

    change default message

--- book_visualizer/tests.py
+++ book_visualizer/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from datetime import date
 
 # Create your tests here.
+class RestTesting(TestCase):
+
+    def setUp(self):
+        name = 'TestingRest'
+        from book_visualizer.models import User
+        self.user = User.objects.create_user(username=name, email='user@example.com', password='TestingPassword123')
+        self.user.save()
+        from book_visualizer.models import Book
+        self.book = Book('1234', 'Some title', 'Author Example', date.today(), 'Publisher Example', 'Summary here')
+        self.book.save()
+    
+    def test_get_comments(self):
+        pass
+
+    
