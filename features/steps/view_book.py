e498efbe0955d06b485cb3b8864465b9ee480c89
Anna Torres Tuca <annattmail@gmail.com>
2020-05-09 11:25:20 +0200

    Testing view book complete

--- features/steps/view_book.py
+++ features/steps/view_book.py
@@ -3,21 +3,26 @@ from datetime import date
 
 use_step_matcher("parse")
 
-@given('Exists a book titled "{title}" and isbn "{isbn}"')
+@given(u'Exists a book titled "{title}" and isbn "{isbn}"')
 def step_impl(context, title, isbn):
     from book_visualizer.models import Book
     book = Book(isbn, title, 'Author Example', date.today(), 'Publisher Example', 'Summary here')
     book.save()
 
-@when('Navigate to book "{isbn}"')
+@when(u'I navigate to book "{isbn}"')
 def step_impl(context, isbn):
     context.browser.visit(context.get_url(f'/book/{isbn}'))
 
-@then('I\'m viewing the book "{title}" with isbn "{isbn}"')
+@when(u'I introduce "{key_word}" into the search bar and click')
+def step_impl(context, key_word):
+     context.browser.visit(context.get_url(f'/search?n={key_word}'))
+
+@then(u'I\'m viewing the book "{title}" with isbn "{isbn}"')
 def step_impl(context, title, isbn):
     assert context.browser.is_text_present(title)
     assert context.browser.is_text_present(isbn)
 
-@given('Exists a book title containing "{Covid19}"')
-def step_impl(context, Covid19):
-    from book_visualizer.models import Book
+@then(u'I can see a book titled "{title}"')
+def step_impl(context, title):
+    assert context.browser.is_text_present(f"{title}")
+
