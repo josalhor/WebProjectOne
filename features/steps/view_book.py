1cd563683edc17363d181285277b84ad18da7437
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-05-09 04:10:06 +0200

    fix view book

--- features/steps/view_book.py
+++ features/steps/view_book.py
@@ -1,17 +1,17 @@
 from behave import *
+from datetime import date
 
 use_step_matcher("parse")
 
 @given('Exists a book titled "{title}" and isbn "{isbn}"')
 def step_impl(context, title, isbn):
     from book_visualizer.models import Book
-    #book = Book.objects.get(title=title)
-    #assert book existeix
-    
+    book = Book(isbn, title, 'Author Example', date.today(), 'Publisher Example', 'Summary here')
+    book.save()
 
 @when('Navigate to book "{isbn}"')
 def step_impl(context, isbn):
-    context.browser.visit(context.get_url('/book/{isbn}'))
+    context.browser.visit(context.get_url(f'/book/{isbn}'))
 
 @then('I\'m viewing the book "{title}" with isbn "{isbn}"')
 def step_impl(context, title, isbn):
