71417562d010a80d409d3701286e2766f5d441c0
bcaspi <balcp11@hotmail.com>
2020-05-09 04:03:09 +0200

    Testing behave

--- features/steps/view_book.py
+++ features/steps/view_book.py
@@ -2,14 +2,14 @@ from behave import *
 
 use_step_matcher("parse")
 
-@given('Exists a book titled {title} and isbn {isbn}')
+@given('Exists a book titled "{title}" and isbn "{isbn}"')
 def step_impl(context, title, isbn):
     from book_visualizer.models import Book
     #book = Book.objects.get(title=title)
     #assert book existeix
     
 
-@when('Navigate to book {isbn}')
+@when('Navigate to book "{isbn}"')
 def step_impl(context, isbn):
     context.browser.visit(context.get_url('/book/{isbn}'))
 
@@ -17,3 +17,7 @@ def step_impl(context, isbn):
 def step_impl(context, title, isbn):
     assert context.browser.is_text_present(title)
     assert context.browser.is_text_present(isbn)
+
+@given('Exists a book title containing "{Covid19}"')
+def step_impl(context, Covid19):
+    from book_visualizer.models import Book
--- features/view_book.feature
+++ features/view_book.feature
@@ -3,10 +3,15 @@ Feature: view details of a book
     As a visitor or user
     I want to view book details and its reviews
 
-    Scenario: view details of a book
+   Scenario: view details of a book
       Given Exists a book titled "GOOP CLEAN BEAUTY" and isbn "9781455541553"
       When Navigate to book "9781455541553"
       Then I'm viewing the book "GOOP CLEAN BEAUTY" with isbn "9781455541553"
 
-    Scenario: view reviews of a book
-      
+    Scenario: Look for a book
+      Given Exists a book title containing "Covid19"
+      When  I introduce "Covid19" into the search bar and click 
+      Then I'm viewing the books whose title contains "scandal"
+      | name                  | isbn            | 
+      | How to stop Covid19   | 3647857463546   | 
+      And There are 1 books
